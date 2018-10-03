cda0c165cb05fd319b31e85472b7f3dd8c474237
Luis Morales Ponce <luis.moralesponce@gmail.com>
2018-10-03 18:19:24 -0500

    hi

--- assets/vida/components/modal.php
+++
@@ -1,8 +0,0 @@
-
-<div class="modalvehicularfix">
-    <div class="container">
-            <div class="embed-container">
-                <iframe width="853" height="480" src="https://www.youtube.com/embed/5U9bj7wWPZI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
-    </div>    
-</div>
